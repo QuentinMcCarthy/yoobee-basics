5afd5222ba4b9e957401d0edf2a5edd11c39c5f5
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-04 11:54:05 +1200

    Working on second task.

--- yoobee-numeric-variable/js/numeric-variable.js
+++ yoobee-numeric-variable/js/numeric-variable.js
@@ -1,4 +1,4 @@
-function initialCode(){
+function taskOneInitialCode(){
 	// Create three variables to store the information needed.
 	var price;
 	var quantity;
@@ -11,11 +11,19 @@ function initialCode(){
 	total = price * quantity;
 
 	// Get the element with an id of cost.
-	var el = document.getElementById('cost');
+	var el = document.getElementById('oldCost');
 	el.textContent = '$' + total;
+
+	// 1. Pls make this work in html
+	// 2. Re-code the above using an object and a function declaration
+
+
+	// NOTE: textContent does not work in IE8 or earlier
+	// You can use innerHTML, but note the security issues on p228-231
+	// el.innerHTML = '$' + total;
 }
 
-function newCode(){
+function taskOneNewCode(){
 	var totalPrice = {
 		price: 5,
 		quantity: 14
@@ -24,19 +32,39 @@ function newCode(){
 	totalPrice.total = totalPrice.price * totalPrice.quantity;
 
 	function getCost(){
-		var el = document.getElementById("cost");
+		var el = document.getElementById("newCost");
 		el.textContent = "$" + totalPrice.total;
 	}
 
 	getCost();
 }
 
-newCode();
+// taskOneInitialCode();
+// taskOneNewCode();
 
-// 1. Pls make this work in html
-// 2. Re-code the above using an object and a function declaration
+function taskTwoInitialCode(){
+	// Create a variable to hold a random number between 1 and 10
+	var randomNum = Math.floor((Math.random() * 10) + 1);
 
+	// Create a variable called el to hold the element whose id attribute has a value of info
+	var el = document.getElementById('oldInfo');
+	// Write the number into that element
+	el.innerHTML = '<h2>random number</h2><p>' + randomNum + '</p>';
 
-// NOTE: textContent does not work in IE8 or earlier
-// You can use innerHTML, but note the security issues on p228-231
-// el.innerHTML = '$' + total;
+	// 1. Please use the random code to print the random number to the console
+	// 2. Please use the random code to print the random number to the dom/html
+	// 3. Use some css and jquery to make the radom number appear in a more interesting
+	// way. I.e Casino style, colors or anything you like
+}
+
+function taskTwoNewCode(){
+	var randInt = (1+Math.floor(Math.random()*10));
+
+	console.log(randInt);
+
+	var el = document.getElementById("newInfo");
+	el.innerHTML = "<h1>Random Number:<br><span id='randInt'>"+randInt+"</span>";
+}
+
+// taskTwoInitialCode();
+taskTwoNewCode();
