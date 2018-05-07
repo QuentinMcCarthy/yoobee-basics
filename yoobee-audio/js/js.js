8d486582674ea5e44e54a54627b077fbc82f2036
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-08 11:33:01 +1200

    Audio functions setup, knob methods and propeties recorded.

--- yoobee-audio/js/js.js
+++ yoobee-audio/js/js.js
@@ -1,18 +1,43 @@
-// var audio;
-//
-// function createAudio(id) {
-//     audio = new Audio('music/'+id+'.mp3');
-//     play();
-// }
-//
-// function play(){
-//     audio.play();
-// }
-//
-// function pause(){
-//     audio.pause();
-// }
+// .knob call:
+// $("").knob({
+// 	"min":0,
+// 	"max":100,
+// 	"step":1,
+// 	"angleOffset":0,
+// 	"angleArc":360,
+// 	"stopper":true,
+// 	"readOnly":false,
+// 	"rotation":clockwise,
+// 	"cursor":false,
+// 	"thickness":0.35,
+// 	"displayInput":true,
+// 	"displayPrevious":false,
+// 	"release":function(){},
+// 	"change":function(){},
+// 	"draw":function(){},
+// 	"cancel":function(){},
+// 	"format":function(){}
+// })
 
 $(document).ready(function(){
-	$(".dial").knob();
+	var audio;
+
+	function createAudio(id){
+		audio = new Audio("music/"+id+".mpc")
+		play();
+	}
+
+	function play(){
+		audio.play();
+	}
+
+	function pause(){
+		audio.pause();
+	}
+
+	$("#audioVol").knob({
+		"angleOffset":-90,
+		"angleArc":180,
+		"displayPrevious":true
+	});
 });
