c1d30c07b3b0045469a6e6a2e8a83460695c52ba
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-08 11:09:01 +1200

    jQuery plugin added, gitignore updated to ignore the folder.

---
+++ yoobee-audio/js/js.js
@@ -0,0 +1,18 @@
+// var audio;
+//
+// function createAudio(id) {
+//     audio = new Audio('music/'+id+'.mp3');
+//     play();
+// }
+//
+// function play(){
+//     audio.play();
+// }
+//
+// function pause(){
+//     audio.pause();
+// }
+
+$(document).ready(function(){
+	$(".dial").knob();
+});
