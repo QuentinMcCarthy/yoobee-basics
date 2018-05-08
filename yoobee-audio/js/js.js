e67ca444a8d7538189a047689a27fa5b48a7d10b
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-09 10:34:35 +1200

    Got audio working; knobs are mostly working. Controls added.

--- yoobee-audio/js/js.js
+++ yoobee-audio/js/js.js
@@ -31,16 +31,40 @@ $(document).ready(function(){
 		audio = new Audio("audio/"+id+".mp3");
 		console.dir(audio);
 		play();
+		trackProgress();
 	}
 
 	// Play the music
 	function play(){
 		audio.play();
+		$("#playButton").addClass("activeControl");
+		$("#pauseButton").removeClass("activeControl");
 	}
 
 	// Pause the music
 	function pause(){
 		audio.pause();
+		$("#playButton").removeClass("activeControl");
+		$("#pauseButton").addClass("activeControl");
+	}
+
+	function setVol(vol){
+		if(typeof audio != "undefined"){
+			audio.volume = vol;
+		}
+	}
+
+	function trackProgress(){
+		// console.log(audio.currentTime);
+		// console.log(audio.duration);
+		
+		$("#audioProg")
+			.trigger("configure",{"max":audio.duration})
+			.val(audio.currentTime)
+			.trigger("change");
+
+
+		setTimeout(trackProgress,100);
 	}
 
 	// Function to create the knobs on the page
@@ -51,15 +75,30 @@ $(document).ready(function(){
 			"angleArc":180,
 			"displayInput":false,
 			"displayPrevious":true,
-			"width":"250"
+			"width":"250",
+			"min":0,
+			"max":1,
+			"step":0.01,
+			"draw":function(){ setVol(this.v)}
 		});
 
 		// Audio progress knob
 		$("#audioProg").knob({
 			"thickness":.2,
-			"width":"150"
-		})
+			"width":"150",
+			"readOnly":true
+		});
+
+		setTimeout(function(){createAudio("jackle_app__fortune_cookie")}, 1000)
 	}
+
+	$("#playButton").click(function(){
+		play();
+	});
+
+	$("#pauseButton").click(function(){
+		pause();
+	});
 
 	// Call functions
 	createKnobs();
