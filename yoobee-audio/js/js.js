f1746c95a8b30f561340c0d92fa5c68d89d1362e
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-09 09:03:22 +1200

    Added comments

--- yoobee-audio/js/js.js
+++ yoobee-audio/js/js.js
@@ -1,58 +1,66 @@
-// .knob call:
-// $("").knob({
-// 	"min":0,
-// 	"max":100,
-// 	"step":1,
-// 	"angleOffset":0,
-// 	"angleArc":360,
-// 	"stopper":true,
-// 	"readOnly":false,
-// 	"rotation":clockwise,
-// 	"cursor":false,
-// 	"thickness":.35,
-// 	"displayInput":true,
-// 	"displayPrevious":false,
-// 	"release":function(){},
-// 	"change":function(){},
-// 	"draw":function(){},
-// 	"cancel":function(){},
-// 	"format":function(){}
-// })
+/*
+.knob call:
+$("").knob({
+	"min":0,
+	"max":100,
+	"step":1,
+	"angleOffset":0,
+	"angleArc":360,
+	"stopper":true,
+	"readOnly":false,
+	"rotation":clockwise,
+	"cursor":false,
+	"thickness":.35,
+	"displayInput":true,
+	"displayPrevious":false,
+	"release":function(){},
+	"change":function(){},
+	"draw":function(){},
+	"cancel":function(){},
+	"format":function(){}
+})
+*/
 
 $(document).ready(function(){
+	// Create audio variable
 	var audio;
 
+	// Function to create new audio
 	function createAudio(id){
+		// Use audio constructor in audio var
 		audio = new Audio("audio/"+id+".mp3");
 		console.dir(audio);
 		play();
 	}
 
+	// Play the music
 	function play(){
 		audio.play();
 	}
 
+	// Pause the music
 	function pause(){
 		audio.pause();
 	}
 
+	// Function to create the knobs on the page
 	function createKnobs(){
+		// Audio volume knob
 		$("#audioVol").knob({
 			"angleOffset":-90,
 			"angleArc":180,
 			"displayInput":false,
 			"displayPrevious":true,
-			"width":"200"
+			"width":"250"
 		});
 
+		// Audio progress knob
 		$("#audioProg").knob({
-			"angleOffset":90,
-			"angleArc":180,
-			"rotation":"anticlockwise",
-			"readOnly":true,
-			"thickness":.2
+			"thickness":.2,
+			"width":"150"
 		})
 	}
 
+	// Call functions
 	createKnobs();
 });
