74b7fa74938d7a0f3eba369d0ad5515d6a8e6141
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-14 09:12:47 +1200

    All audio files set up

--- yoobee-audio/js/js.js
+++ yoobee-audio/js/js.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
 
 	// Create variable to keep track of how much
 	// Audio is listed
-	var maxTracks = 3;
+	var maxTracks = 10;
 
 	// Function to create new audio
 	function createAudio(hosted,id){
@@ -143,7 +143,77 @@ $(document).ready(function(){
 
 				// Dropbox hosted
 				createAudio(true,"https://dl.dropboxusercontent.com/s/agya40507f4s3g5/edge_of_infinity_%28minnesota_remix%29.mp3")
-			})
+			}, 1000);
+		}
+		else if(currentTrack==4){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"flash_funk_(marshmello_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/4h8sxxsu7u2rd0d/flash_funk_%28marshmello_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==5){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"let_the_games_begin_(hyper_potions_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/iw058ae68kfoa3a/let_the_games_begin_%28hyper_potions_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==6){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"lucidity_(dan_negovan_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/6q80d9o0hbrmozf/lucidity_%28dan_negovan_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==7){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"silver_scrapes_(protoshredanoid_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/e4na3iu3qcdvn1k/silver_scrapes_%28protoshredanoid_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==8){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"the_glory_(james_egbert_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/f9c43zdc0g3a9ok/the_glory_%28james_egbert_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==9){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"welcome_to_planet_urf_(jauz_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/mbh524zux40yxyk/welcome_to_planet_urf_%28jauz_remix%29.mp3")
+			}, 1000);
+		}
+		else if(currentTrack==10){
+			// Play new audio
+			setTimeout(function(){
+				// Locally hosted
+				// createAudio(false,"worlds_collide_(arty_remix)")
+
+				// Dropbox hosted
+				createAudio(true,"https://dl.dropboxusercontent.com/s/uw4rwd4iqpgdst4/worlds_collide_%28arty_remix%29.mp3")
+			}, 1000);
 		}
 	}
 
