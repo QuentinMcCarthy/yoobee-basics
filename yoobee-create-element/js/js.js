8de5b35493b14acb18d9c6e956d9d2ca4ae96a47
quentin mccarthy <845868401@bun2-206-14.local>
2018-05-17 09:14:14 +1200

    create-element-js complete

--- yoobee-create-element/js/js.js
+++ yoobee-create-element/js/js.js
@@ -1,30 +1,51 @@
 (function() {
-	// Target reference for inserted HTML
-	var htmlTarget = document.getElementsByTagName("script")[0]
+	function addElementToDOM(tag,location,id,style,text){
+		// Define location to put element
+		var htmlTarget = location;
 
-	// Create div element
-	var div = document.createElement("div");
+		// Create the element
+		var createdElement = document.createElement(tag);
 
-	// Insert div element before the target
-	htmlTarget.before(div);
+		// If tree for where to put the element
+		htmlTarget.prepend(createdElement);
 
-	// Set attributes of element
-	div.setAttribute("id","createdDiv");
-	div.setAttribute("style","width:880px; height: 160px; border:1px solid lightblue; border-radius:10px; margin-top:50px;")
+		// Set the attributes of the element
+		createdElement.setAttribute("id",id);
+		createdElement.setAttribute("style",style);
 
-	// Target reference for inserted HTML
-	htmlTarget = div
+		// Put text in the element if not empty
+		if(text != ""){
+			createdElement.innerText = text;
+		}
 
-	// Create p element
-	var p = document.createElement("p");
+		// Return the created element
+		return createdElement;
+	}
 
-	// Insert text into p element
-	p.innerText = "I tried to find the rhythm of the world where I used to live. I followed the current. I was silent, attentive, I made a conscious effort to smile, nod, stand, and perform the millions of gestures that constitute life on earth."
+	// Variables for the various inputs for the function
+	var tagName;
+	var locationTarget;
+	var idAttribute;
+	var styleAttribute;
+	var textInner;
 
-	// Insert p element inside the target
-	htmlTarget.appendChild(p);
+	// Setting variables
+	tagName = "div";
+	locationTarget = document.body;
+	idAttribute = "createdDiv";
+	styleAttribute = "width:880px; height: 160px; border:1px solid lightblue; border-radius:10px; margin-top:50px;";
+	textInner = "";
 
-	// Set attributes of element
-	p.setAttribute("id","createdP");
-	p.setAttribute("style", "font-size:1.9em; margin:10px; font-family:sans-serif; color:#555")
+	// Sets the return from the function as a new variable
+	var newTarget = addElementToDOM(tagName,locationTarget,idAttribute,styleAttribute,textInner);
+
+	// Setting variables
+	tagName = "p";
+	locationTarget = newTarget
+	idAttribute = "createdP";
+	styleAttribute = "font-size:1.9em; margin:10px; font-family:sans-serif; color:#555";
+	textInner = "I tried to find the rhythm of the world where I used to live. I followed the current. I was silent, attentive, I made a conscious effort to smile, nod, stand, and perform the millions of gestures that constitute life on earth.";
+
+	// Calling the function with new variables
+	addElementToDOM(tagName,locationTarget,idAttribute,styleAttribute,textInner);
 }());
