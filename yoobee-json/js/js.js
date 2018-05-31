02e30e7e95ff2c048f96f4b20500b70b430ccca3
quentin mccarthy <845868401@bun2-206-14.local>
2018-06-01 11:51:42 +1200

    Introduction to JSON

---
+++ yoobee-json/js/js.js
@@ -0,0 +1,19 @@
+var arnoldSuits = {
+	basic:{
+		pricing:100,
+		pricingInterval:"1day",
+		sizes:[40,41,42,43,44,45,46]
+	},
+	tuxedo:{
+		pricing:160,
+		pricingInterval:"1day",
+		sizes:[42,43,44,45,46,47,48,49,50]
+	},
+	armani:{
+		pricing:400,
+		pricingInterval:"1day",
+		sizes:[48,49,40,41,42,43,44,45,46]
+	}
+};
+
+console.log(arnoldSuits);
