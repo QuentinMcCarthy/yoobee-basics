0c7a1ad9a357f665f3c72c138f3ed2887b43e0be
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-07 10:08:19 +1200

    Added markers to the map

--- yoobee-googlemaps/js/script.js
+++ yoobee-googlemaps/js/script.js
@@ -1,12 +1,14 @@
 google.maps.event.addDomListener(window, "load", initMap);
 
+var map;
+
 function initMap(){
 	var mapOptions = {
 		center: {
 			lat: -41.279214,
 			lng: 174.780340
 		},
-		zoom: 13,
+		zoom: 14,
 		disableDefaultUI: true,
 		fullscreenControl: true,
 		fullscreenControlOptions: {
@@ -77,5 +79,40 @@ function initMap(){
 		]
 	}
 
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+	addAllMarkers();
+}
+
+function dropMarker(place,num,interval){
+	setTimeout(function(){
+		$("#placeList").append("<div class='placeList-item'><h3>"+place.placeName+"</h3></div><hr>");
+
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(place.lat, place.lng),
+			title: place.placeName,
+			map: map,
+			animation: google.maps.Animation.DROP,
+			label: num.toString()
+		});
+	}, interval);
+}
+
+function addAllMarkers(){
+	$.ajax({
+		type: "GET",
+		url: "data/markers.json",
+		dataType: "json",
+		success: function(data){
+			console.log(data);
+
+			for(var i = 0; i < data.length; i++){
+				dropMarker(data[i], i + 1, i * 250);
+			}
+		},
+		error: function(err){
+			console.log("Error "+err.status);
+			console.log(err);
+		}
+	});
 }
