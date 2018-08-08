f5bc7f7df41bdaa0f253e0728a3fe73f0e127d5e
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-09 11:48:55 +1200

    Geocoding with inputs for routes

--- yoobee-googlemaps/directions/js/script.js
+++ yoobee-googlemaps/directions/js/script.js
@@ -1,13 +1,16 @@
 google.maps.event.addDomListener(window, "load", initMap);
 
 var directionsService = new google.maps.DirectionsService(),
-	directionsDisplay,
-	map;
+	directionsDisplay, map;
+
+const wellington = new google.maps.LatLng(-41.286859, 174.775807);
+const auckland = new google.maps.LatLng(-36.8485, 174.7633);
+const taupo = new google.maps.LatLng(-38.6857, 176.0702);
+const tauranga = new google.maps.LatLng(-37.687882, 176.165172);
+const newPlymouth = new google.maps.LatLng(-39.057007, 174.075015);
 
 function initMap(){
 	directionsDisplay = new google.maps.DirectionsRenderer();
-
-	const wellington = new google.maps.LatLng(-41.286859, 174.775807);
 
 	const mapOptions = {
 		zoom: 6,
@@ -19,20 +22,62 @@ function initMap(){
 
 	directionsDisplay.setMap(map);
 
-	getRoute(wellington);
+	// getRoute(wellington, newPlymouth);
+
+	initForm();
 }
 
-function getRoute(start){
-	var waypts = [];
+function initForm(){
+	$("#placeRequest").submit(function(){
+		event.preventDefault();
 
-	const stop = new google.maps.LatLng(-38.6857, 176.0702);
-	const end = new google.maps.LatLng(-36.8485, 174.7633);
+		getOrigin();
+	})
+}
 
-	waypts.push({
-		location: stop,
-		stopover: true
-	});
+function getOrigin(){
+	var geocoder = new google.maps.Geocoder();
 
+	var start;
+
+	geocoder.geocode({
+		address: $("input[name='origin']").val()
+	}, function(results, status){
+		if(status === google.maps.GeocoderStatus.OK){
+			console.log(results);
+
+			start = results[0].geometry.location;
+
+			getDestination(start);
+		}else{
+			console.log("Something went wrong");
+			console.log(status);
+		}
+	})
+}
+
+function getDestination(start){
+	var geocoder = new google.maps.Geocoder()
+
+	var end;
+
+	geocoder.geocode({
+		address: $("input[name='destination']").val()
+	}, function(results, status){
+		if(status === google.maps.GeocoderStatus.OK){
+			console.log(results);
+
+			end = results[0].geometry.location;
+
+			getRoute(start, end);
+		}else{
+			console.log("Something went wrong");
+			console.log(status);
+		}
+	})
+}
+
+function getRoute(start, end){
 	const request = {
 		origin: start,
 		destination: end,
@@ -44,8 +89,6 @@ function getRoute(start){
 	directionsService.route(request, function(response,status){
 		if(status === google.maps.DirectionsStatus.OK){
 			directionsDisplay.setDirections(response);
-
-			let route = response.routes[0];
 		}else{
 			console.log("Something went wrong");
 			console.log(status);
